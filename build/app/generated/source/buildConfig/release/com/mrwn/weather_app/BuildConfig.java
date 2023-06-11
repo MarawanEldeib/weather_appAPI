3524a0c00a95e36185175b6a57b0c2ccb910531b
Marawan <105850133+Marawan2002@users.noreply.github.com>
2023-06-11 18:54:44 +0800

    debug apk

---
+++ build/app/generated/source/buildConfig/release/com/mrwn/weather_app/BuildConfig.java
@@ -0,0 +1,12 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package com.mrwn.weather_app;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = false;
+  public static final String APPLICATION_ID = "com.mrwn.weather_app";
+  public static final String BUILD_TYPE = "release";
+  public static final int VERSION_CODE = 1;
+  public static final String VERSION_NAME = "1.0.0";
+}
